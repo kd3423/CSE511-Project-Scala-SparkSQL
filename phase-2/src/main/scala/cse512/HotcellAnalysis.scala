2b99ded256d767bbf97606c1511b0a07c4a214d7
aqsinha <amitsinha12766@gmail.com>
2021-04-17 16:40:26 -0700

    Changes for HotCell analysis

--- phase-2/src/main/scala/cse512/HotcellAnalysis.scala
+++ phase-2/src/main/scala/cse512/HotcellAnalysis.scala
@@ -12,7 +12,7 @@ object HotcellAnalysis {
   Logger.getLogger("com").setLevel(Level.WARN)
 
 def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
-{
+ {
   // Load the original data from a data source
   var pickupInfo = spark.read.format("com.databricks.spark.csv").option("delimiter",";").option("header","false").load(pointPath);
   pickupInfo.createOrReplaceTempView("nyctaxitrips")
@@ -44,6 +44,31 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
 
   // YOU NEED TO CHANGE THIS PART
 
-  return pickupInfo // YOU NEED TO CHANGE THIS PART
-}
+  //Create a temporary view pickupInfo
+  pickupInfo.createOrReplaceTempView("pickupInfoTempView")
+
+  // Function to check whether the point is inside the cube boundary
+  spark.udf.register("isCellWithinTheCube", (x: Double, y:Double, z:Double) => ( (x >= minX) && (x<=maxX) && (y>minY) && (y<=maxY) && (z >=minZ) && (z<=maxZ)))
+
+  //Get all the points which are in a given boundary
+  var insideBoundaryPoints = spark.sql("select x,y,z from pickupInfoTempView where isCellWithinTheCube(x,y,z) order by z,y,x").persist()
+  insideBoundaryPoints.createOrReplaceTempView("insideBoundaryPointsTempView")
+
+  //Count all the pickups from same cell
+  val countOfPointsInACell = spark.sql("select x,y,z,count(*) as numPoints from insideBoundaryPointsTempView group by z,y,x order by z,y,x").persist()
+  countOfPointsInACell.createOrReplaceTempView("countOfPointsInACellTempView")
+
+  //Define
+  spark.udf.register("squareInput", (x: Int) => (x*x).toDouble)
+  val sumOfPoints = spark.sql("select count(*) as numberOfCellsWithAtleastOnePoint, sum(numPoints) as totalPointsInAGivenRegion, sum(squared(numPoints)) as squaredSumOfAllPointsInGivenRegion from countOfPointsInACell")
+  sumOfPoints.createOrReplaceTempView("sumOfPoints")
+
+   //val numberOfCellsWithAtleastOnePoint = sumOfPoints.first().getLong(0)
+   val totalPoints = sumOfPoints.first().getLong(1)
+   val squaredSumCells = sumOfPoints.first().getDouble(2)
+
+   val XMean = totalPoints/ numCells
+   val StdDev = math.sqrt((squaredSumCells / numCells) - (XMean*XMean))
+   return pickupInfo // YOU NEED TO CHANGE THIS PART
+ }
 }
