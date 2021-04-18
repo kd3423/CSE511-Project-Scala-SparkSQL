ac9f5c48bbdccd8bc7694508cf8f8da6f12c3058
aqsinha <amitsinha12766@gmail.com>
2021-04-18 09:52:44 -0700

    Fixed HotZoneUtils

--- phase-2/src/main/scala/cse512/HotzoneUtils.scala
+++ phase-2/src/main/scala/cse512/HotzoneUtils.scala
@@ -4,18 +4,23 @@ object HotzoneUtils {
 
   def ST_Contains(queryRectangle: String, pointString: String ): Boolean = {
     // YOU NEED TO CHANGE THIS PART
-    if (!queryRectangle.isEmpty && !pointString.isEmpty){
-      val recPts = queryRectangle.split(",").map(_.toDouble)
-      val pt = pointString.split(",").map(_.toDouble)
+    if (queryRectangle == null || pointString == null || queryRectangle.isEmpty || pointString.isEmpty)
+         return false
 
-      if(pt(0)<=recPts(2) && pt(0)>=recPts(0) && pt(1)>=recPts(1) && pt(1)<=recPts(3)) return true
-      else if(pt(0)>=recPts(2) && pt(0)<=recPts(0) && pt(1)<=recPts(1) && pt(1)>=recPts(3)) return true
-      else return false
-    }
+    val recPts = queryRectangle.split(",")
+    val pt = pointString.split(",")
 
-    return false// YOU NEED TO CHANGE THIS PART
+    val rectx1 = queryRectangle(0).toDouble
+    val recty1 = queryRectangle(1).toDouble
+    val rectx2 = queryRectangle(2).toDouble
+    val recty2 = queryRectangle(3).toDouble
+
+    val x1 = pt(0).toDouble
+    val y1 = pt(1).toDouble
+
+    if(x1<=rectx2 && x1>=rectx1 && y1>=recty1 && y1<=recty2) return true
+    else if(x1>=rectx2 && x1<=rectx1 && y1 <=recty1 && y1>=recty2) return true
+    else return false
+
   }
-
-  // YOU NEED TO CHANGE THIS PART
-
 }
